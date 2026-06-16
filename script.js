db857a9f59ecd4b13d23bc321dec554a9aeb29a0
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-06-16 19:39:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Person {}
+class Person {
+	name;
+	age;
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	} 
+	get(){
+		return this.name;
+	}
+	set(age){
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${Person.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
